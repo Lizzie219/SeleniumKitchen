5151d299c16987d3462bbda32646be7c34ecf8a8
Lizzie219 <cs.lizett9@stud.uni-obuda.hu>
2025-06-01 20:22:24 +0200

    unused imports removed

--- tests/src/test/java/pages/Page.java
+++ tests/src/test/java/pages/Page.java
@@ -2,13 +2,10 @@ package pages;
 
 import config.Config;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 public abstract class Page {
 	private static final By BODY_LOCATOR = By.tagName("body");
